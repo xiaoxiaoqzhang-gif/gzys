47075db85a48f0fb79505ab221694d3ee1dc206f
xiaoxiaoqzhang-gif <xiaoxiaoqzhang@gmail.com>
2026-07-16 00:39:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var rule = {
+    title: "测试源",
+    host: "https://www.baidu.com",
+    url: "/",
+    searchable: 0,
+    quickSearch: 0,
+    filterable: 0,
+    class_name: "测试",
+    class_url: "1"
+};
+
+module.exports = rule;
